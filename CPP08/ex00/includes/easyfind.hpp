636fe511b36df5fc7d971c60ca54a583d14fbf35
Ahmed Aarij <ahaarij@lab1r1s3.42abudhabi.ae>
2025-02-12 09:59:28 +0400

    Pushing ex00 CPP08

---
+++ CPP08/ex00/includes/easyfind.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <exception>
+
+class NotFoundException: public std::exception
+{
+	public:
+            virtual const char  *what() const throw()
+			{
+				return ("Number not found!");
+			}
+};
+
+
+template <typename T>
+typename T::iterator easyfind(T &container, int n)
+{
+	typename T::iterator iter;
+
+	iter = std::find(container.begin(), container.end(), n);
+	if(iter == container.end())
+		throw NotFoundException();
+	return (iter);
+}
---
+++ CPP08/ex00/srcs/main.cpp
@@ -0,0 +1,18 @@
+#include "../includes/easyfind.hpp"
+#include <vector>
+
+int	main()
+{
+	std::vector<int> v;
+	for(int i = 0; i < 10; i ++)
+		v.push_back(i);
+	try
+	{
+		std::cout << *easyfind(v, 5) << std::endl;
+		std::cout << *easyfind(v, 500) << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
